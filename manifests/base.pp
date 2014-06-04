5e346fa30fcdffd63fdfdcc09335fbcec919aeaa
cleverlight <github.account@cleveraspaint.com>
2014-06-04 19:43:42 +0100

    working nodejs including forever and monitoring

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,12 +5,14 @@ define donodejs::base (
   # setup defaults
 
   $user = 'web',
+  $group = 'www-data',
   $port = 3000,
   $app_name = $title,
+  $app_script = 'bin/www',
   $content = 'Express',
 
   # install directory
-  $target_dir = '/var/www/html',
+  $target_dir = '/var/www/node',
 
   $firewall = true,
   $monitor = true,
@@ -39,19 +41,52 @@ define donodejs::base (
       check_command => "check_http_port_url_content!${::ipaddress}!${port}!/!'${content}'",
     }
     @nagios::service { "int:process_node-donodejs-${::fqdn}":
-      check_command => "check_procs!1:!1:!node",
+      check_command => "check_nrpe_procs_postfix",
     }
   }
 
   # if we've got a message of the day, include
   @domotd::register { "Node(${port})" : }
 
+  # check that target dir exists
+  if ! defined(File["${target_dir}"]) {
+    docommon::stickydir { "${target_dir}":
+      user => $user,
+      group => $group,
+      context => 'httpd_sys_content_t',
+      before => [Exec["donodejs-base-create-${title}"]],
+    }
+  }
+
   # create and inflate node/express example
   exec { "donodejs-base-create-${title}" :
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
     command => "express ${target_dir}/${app_name} && cd ${target_dir}/${app_name} && npm install",
     user => $user,
+    group => $group,
     cwd => "/home/${user}",
+  }
+
+  # create service and start on machine startup
+  case $operatingsystem {
+    centos, redhat, fedora: {
+    }
+    ubuntu, debian: {
+    }
+  }
+  # experimenting with symmetric file resource for both CentOS and Ubuntu
+  file { "donodejs-base-service-script-${title}":
+    name => "/etc/init.d/${app_name}",
+    content => template('donodejs/service.erb'),
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+  service { "donodejs-base-service-${title}":
+    name => "${app_name}",
+    enable => true,
+    ensure => 'running',
+    require => [Exec["donodejs-base-create-${title}"], File["donodejs-base-service-script-${title}"]],
   }
 
   # create symlink from our home folder
